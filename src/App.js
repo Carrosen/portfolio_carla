9e40a2f2de71074569dc0ba580281158a6af9ce8
Carrosen <carla1.rosen@gmail.com>
2019-07-08 19:49:27 +0200

    Installs react router, creates component for landing page, adds route for component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
-import React from 'react';
-import './App.css';
+import React, { Component } from 'react'
+import { Switch, Route } from 'react-router-dom'
+import LandingPage from './Components/LandingPage'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        hello world, bitch!
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  render() {
+    return (
+      <>
+        <Switch>
+          <Route exact path='/' component={LandingPage}></Route>
+        </Switch>
+      </>
+    );
+  }
 }
 
-export default App;
+export default App
---
+++ src/Components/LandingPage.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class LandingPage extends Component {
+  
+  render() {
+    return (
+      <>
+      <h1>Hello sis!</h1>
+      </>
+    )
+  }
+}
+
+export default LandingPage
