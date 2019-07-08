b3a00615286b78f154938334e9690c17b103337a
Carrosen <carla1.rosen@gmail.com>
2019-07-08 19:52:55 +0200

    Creates components for all views, adds Header component with links to different views

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 import { Switch, Route } from 'react-router-dom'
 import LandingPage from './Components/LandingPage'
+import About from './Components/About'
+import Contact from './Components/Contact'
+import Header from './Components/Header'
+import Portfolio from './Components/Portfolio'
 
 class App extends Component {
 
   render() {
     return (
       <>
+      <Header />
         <Switch>
           <Route exact path='/' component={LandingPage}></Route>
+          <Route exact path='/portfolio' component={Portfolio}></Route>
+          <Route exact path='/about' component={About}></Route>
+          <Route exact path='/contact' component={Contact}></Route>
         </Switch>
       </>
     );
