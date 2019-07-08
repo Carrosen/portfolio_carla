7d61f588e5ca2aaad856b99f7100248a10f220a5
Carrosen <carla1.rosen@gmail.com>
2019-07-08 23:41:45 +0200

    Adds icons with external links to github and linkedin

--- src/Components/LandingPage.jsx
+++ src/Components/LandingPage.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Container } from 'semantic-ui-react'
+import { Container, Icon, Item } from 'semantic-ui-react'
 
 class LandingPage extends Component {
   
@@ -10,6 +10,27 @@ class LandingPage extends Component {
         <h1>Carla Rosén // portfolio</h1>
           <p>Carla Rosén</p>
           <p>web developer</p>
+
+          <Item
+            href="https://linkedin.com"
+            id='icon-linkedin'
+          >
+            <Icon
+              name="linkedin square"
+              size="big"
+            />
+          </Item>
+
+          <Item
+            href="https://github.com/Carrosen"
+            id='icon-github'
+          >
+            <Icon
+              name="github square"
+              size="big"
+            />
+          </Item>
+
       </Container>
       </>
     )
