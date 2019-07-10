c36089facc68a63d0eac1693fe5158793bd5048c
Carrosen <carla1.rosen@gmail.com>
2019-07-10 21:11:23 +0200

    Creates card in portfolio component, adds image

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -1,13 +1,26 @@
 import React, { Component } from 'react'
-import { Container } from 'semantic-ui-react'
+import { Container, Card, Image } from 'semantic-ui-react'
+import myImage from '../cooper.png'
 
 class Portfolio extends Component {
-  
+
   render() {
     return (
       <>
         <Container>
-          <h1>Portfolio</h1>
+          <h1>portfolio</h1>
+          <p>Click on the cards to go to the application</p>
+
+          <Card>
+            <Image src={myImage} wrapped ui={false} />
+            <Card.Content>
+              <Card.Description>
+                Cooper
+              </Card.Description>
+            </Card.Content>
+          </Card>
+
+
         </Container>
       </>
     )
