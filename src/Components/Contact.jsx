6989272ff87d1af4970d21d62237d83506d00170
Carrosen <carla1.rosen@gmail.com>
2019-08-20 20:06:16 +0200

    Adds text and minor styling

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Container } from 'semantic-ui-react'
+import { Container, Divider } from 'semantic-ui-react'
 
 class Contact extends Component {
   
@@ -9,6 +9,11 @@ class Contact extends Component {
         <Container>
           <div className='text'>
             <h1>Contact</h1>
+
+            <br></br>
+            <Divider></Divider>
+            <br></br>
+
             <p>If you would like to get in touch with me, please contact me here: carla1.rosen@gmail.com</p>
           </div>
         </Container>
--- src/Components/LandingPage.jsx
+++ src/Components/LandingPage.jsx
@@ -1,38 +1,54 @@
 import React, { Component } from 'react'
-import { Container, Icon, Item, Image } from 'semantic-ui-react'
+import { Container, Icon, Item, Image, Grid } from 'semantic-ui-react'
 import cr from '../cr.png'
 
 class LandingPage extends Component {
-  
+
   render() {
     return (
       <>
-        <Container>
-          <div className='portfolio-text'>
-              <p>CARLA ROSÉN</p>
-              <Image src={cr} wrapped ui={false} />
-          </div>
+        <Container id='landing-page'>
+          <Grid columns={2}>
+            <Grid.Column width={10}>
+              <div className='portfolio-text'>
+                <p>Hi, I'm Carla</p>
+                <p>Jr. Full Stack Developer</p>
+              </div>
+              <Item
+                href="https://www.linkedin.com/in/carla-ros%C3%A9n-9a4903189/"
+                id='icon-linkedin'
+              >
+                <Icon
+                  name="linkedin square"
+                  size="big"
+                />
+              </Item>
 
-            <Item
-              href="https://www.linkedin.com/in/carla-ros%C3%A9n-9a4903189/"
-              id='icon-linkedin'
-            >
-              <Icon
-                name="linkedin square"
-                size="big"
-              />
-            </Item>
+              <Item
+                href="https://github.com/Carrosen"
+                id='icon-github'
+              >
+                <Icon
+                  name="github square"
+                  size="big"
+                />
+              </Item>
+            </Grid.Column>
 
-            <Item
-              href="https://github.com/Carrosen"
-              id='icon-github'
-            >
-              <Icon
-                name="github square"
-                size="big"
-              />
-            </Item>
+            <Grid.Column width={6}>
+              <br></br>
+              <div id='profile-image'>
+                <Image src={cr} wrapped ui={false} />
+              </div>
+            </Grid.Column>
+          </Grid>
+
+
+
         </Container>
+        <div id='color-block'>
+          <p>hi</p>
+        </div>
       </>
     )
   }
